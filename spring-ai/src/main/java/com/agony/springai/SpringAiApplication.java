eb96aee316d902e9dd6651fb80ac126d83c55cb1
Agony <gcl1197134883@gmail.com>
2026-03-15 11:30:08 +0800

    initial commit

---
+++ spring-ai/src/main/java/com/agony/springai/SpringAiApplication.java
@@ -0,0 +1,16 @@
+package com.agony.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Agony
+ * @create: 2026/3/9 21:34
+ * @describe:
+ */
+@SpringBootApplication
+public class SpringAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiApplication.class, args);
+    }
+}
---
+++ spring-ai/src/main/java/com/agony/springai/controller/ChatCompareDemo.java
@@ -0,0 +1,35 @@
+package com.agony.springai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+
+/**
+ * @author: Agony
+ * @create: 2026/3/15 11:15
+ * @describe:
+ */
+public class ChatCompareDemo {
+
+    private final ChatClient chatClient;
+
+    private final ChatModel chatModel;
+
+    public ChatCompareDemo(ChatClient chatClient, ChatModel chatModel) {
+        this.chatClient = chatClient;
+        this.chatModel = chatModel;
+    }
+
+    public void compare() {
+
+        // use model
+        Prompt prompt = new Prompt(new UserMessage("hello"));
+        ChatResponse call = chatModel.call(prompt);
+        String text1 = call.getResult().getOutput().getText();
+
+        // user client
+        String text2 = chatClient.prompt().user("hello").call().content();
+    }
+}
