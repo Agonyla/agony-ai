00878144bc0ab7ddf7614efd38c1ab9e0a33089a
Agony <gcl1197134883@gmail.com>
2026-05-08 19:47:15 +0800

    feat: sse流式响应

---
+++ spring-ai/src/main/java/com/agony/springai/controller/sse/StreamController.java
@@ -0,0 +1,41 @@
+package com.agony.springai.controller.sse;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author: Agony
+ * @create: 2026/5/8 19:14
+ * @describe:
+ */
+@RestController
+@RequestMapping("/stream")
+public class StreamController {
+
+    private final ChatClient chatClient;
+
+    public StreamController(ChatClient.Builder builder) {
+        this.chatClient = builder.defaultSystem("你是一个Java助手").build();
+    }
+
+    /**
+     * 流式对话接口
+     * produces = TEXT_EVENT_STREAM_VALUE 告诉 Spring 这是 SSE 响应
+     *
+     * @param message message
+     * @return Flux<String>
+     */
+    @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamChat(@RequestParam String message) {
+
+        return chatClient.prompt()
+                .user(message)
+                .stream()
+                .content();
+    }
+}
