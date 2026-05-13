b9e1bbc2a3dfa5a871107352e9d19452461d8a4a
Agony <gcl1197134883@gmail.com>
2026-05-13 14:02:01 +0800

    spring-ai-alibaba initial commit

---
+++ spring-ai-alibaba/src/main/java/com/agony/springaialibaba/SpringAiAlibabaApplication.java
@@ -0,0 +1,18 @@
+package com.agony.springaialibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Agony
+ * @create: 2026/5/13 13:43
+ * @describe:
+ */
+@SpringBootApplication
+public class SpringAiAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiAlibabaApplication.class, args);
+    }
+
+}
