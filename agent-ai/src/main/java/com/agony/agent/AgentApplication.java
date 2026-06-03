fdcbc9c80dc8fa0de2d6fc041e40b0f5c7b38a7d
Agony <gcl1197134883@gmail.com>
2026-06-03 18:26:12 +0800

    agent-ai initial commit

---
+++ agent-ai/src/main/java/com/agony/agent/AgentApplication.java
@@ -0,0 +1,17 @@
+package com.agony.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Agony
+ * @create: 2026/6/3 18:17
+ * @describe:
+ */
+@SpringBootApplication
+public class AgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentApplication.class, args);
+    }
+}
