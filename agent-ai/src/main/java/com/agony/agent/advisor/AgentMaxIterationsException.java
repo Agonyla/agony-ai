41fcbf5a1930535c86febe98e5ad8c37b6a035d1
Agony <gcl1197134883@gmail.com>
2026-06-15 15:53:31 +0800

    feat: 生成可靠性——防止循环

---
+++ agent-ai/src/main/java/com/agony/agent/advisor/AgentMaxIterationsException.java
@@ -0,0 +1,12 @@
+package com.agony.agent.advisor;
+
+/**
+ * @author: Agony
+ * @create: 2026/6/15 15:50
+ * @describe:
+ */
+public class AgentMaxIterationsException extends RuntimeException {
+    public AgentMaxIterationsException(String message) {
+        super(message);
+    }
+}
