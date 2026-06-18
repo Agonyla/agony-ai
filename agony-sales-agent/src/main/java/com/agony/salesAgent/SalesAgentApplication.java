c6858388f5f73031e06cfef09484f7278e2da1ab
Agony <gcl1197134883@gmail.com>
2026-06-18 12:33:11 +0800

    agony-sales-agent initial commit

---
+++ agony-sales-agent/src/main/java/com/agony/salesAgent/SalesAgentApplication.java
@@ -0,0 +1,16 @@
+package com.agony.salesAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Agony
+ * @create: 2026/6/18 11:19
+ * @describe:
+ */
+@SpringBootApplication
+public class SalesAgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SalesAgentApplication.class, args);
+    }
+}
