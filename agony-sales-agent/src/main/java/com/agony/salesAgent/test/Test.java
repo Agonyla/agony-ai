429066e0cec895a8ebb727c405413e1c9ff5cfa0
Agony <gcl1197134883@gmail.com>
2026-07-17 14:34:50 +0800

    feat: stream流测试

--- agony-sales-agent/src/main/java/com/agony/salesAgent/test/Test.java
+++ agony-sales-agent/src/main/java/com/agony/salesAgent/test/Test.java
@@ -1,6 +1,7 @@
 package com.agony.salesAgent.test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @author: Agony
@@ -15,6 +16,25 @@ public class Test {
         LocalDate date2 = date;
         System.out.println(date2.isBefore(date));
 
+        List<User> users = List.of(new User("jack", 29),
+                new User("marry", 20),
+                new User("ted", 47));
+
+        int totalAge = users.stream()
+                .mapToInt(User::age)
+                .sum();
+
+        Integer sumAge = users.stream()
+                .map(User::age)
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println(totalAge);
+        System.out.println(sumAge);
+    }
+
+    public record User(
+            String name,
+            int age) {
     }
 
 }
